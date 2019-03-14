ab2e488d8faa621236294e018c96e5bbec605760
Jon <jonathan.woff@gmail.com>
2019-03-15 01:04:35 +1100

    Fixing WCF service

--- Calculator/Service.cs
+++ Calculator/Service.cs
@@ -3,26 +3,26 @@ using System.ServiceModel;
 
 namespace Calculator
 {
-    [ServiceContract]
     public class Service : IService
     {
-        [OperationContract]
         public int Add(int num1, int num2)
         {
             return num1 + num2;
         }
 
-        [OperationContract]
         public string Help()
         {
             return $"{Environment.MachineName}";
         }
     }
 
+    [ServiceContract]
     public interface IService
     {
+        [OperationContract]
         int Add(int num1, int num2);
 
+        [OperationContract]
         string Help();
     }
 }
